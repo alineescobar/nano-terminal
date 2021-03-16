3adaacc2bcd1908854954e61b43b8465cf501065
Júlia Silveira de Souza <70410764+JuhbsSouza@users.noreply.github.com>
2021-03-16 15:42:13 -0300

    Teste de commit

--- nano-terminal/main.swift
+++ nano-terminal/main.swift
@@ -2,9 +2,10 @@
 //  main.swift
 //  nano-terminal
 //
-//  Created by Aline Osana Escobar on 16/03/21.
+//  Created by Aline Osana Escobar and Julia Silveira de Souza on 16/03/21.
 //
 
 import Foundation
 
 print("hello")
+
